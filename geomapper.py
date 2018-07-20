c576af7153a5cf6c32a5a34aa397cba8e219719e
voreshem <cj.hague@protonmail.com>
2018-07-20 07:43:18 -0400

    enabled satellite mapping

--- geomapper.py
+++ geomapper.py
@@ -2,7 +2,7 @@ import json
 import pandas as pd
 import matplotlib.pyplot as plt
 import cartopy.crs as ccrs
-from cartopy.io.img_tiles import OSM
+from cartopy.io.img_tiles import GoogleTiles
 
 
 with open("2014_10_20.json") as f:
@@ -24,8 +24,8 @@ latlng_df = pd.DataFrame(hike_df.latlng.values.tolist(), columns=['lat', 'lon'])
 hikedf = pd.concat([hike_df, latlng_df], axis=1)
 
 def main():
-    img = OSM()
-    plt.figure(figsize=(60,60))
+    img = GoogleTiles(url='https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}.jpg')
+    plt.figure(figsize=(100,100))
     ax = plt.axes(projection=img.crs)
 
     ax.set_extent([-80.127383, -80.095359, 26.911549, 26.883035])
@@ -34,7 +34,7 @@ def main():
 
     x, y = hikedf.lon, hikedf.lat
 
-    ax.plot(x, y, '.', transform=ccrs.Geodetic(), color='red')
+    ax.plot(x, y, transform=ccrs.Geodetic(), color='blue', linewidth=3)
 
     plt.savefig("test_map.png")
 
