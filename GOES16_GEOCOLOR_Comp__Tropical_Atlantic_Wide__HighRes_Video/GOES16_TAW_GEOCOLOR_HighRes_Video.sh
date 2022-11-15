cdc36e3685fffab065d9d2f5a967e15bd5b4733d
cj.hague <voreshem@users.noreply.github.com>
2022-11-14 23:27:22 -0500

    Fixed outputs.

--- GOES16_GEOCOLOR_Comp__Tropical_Atlantic_Wide__HighRes_Video/GOES16_TAW_GEOCOLOR_HighRes_Video.sh
+++ GOES16_GEOCOLOR_Comp__Tropical_Atlantic_Wide__HighRes_Video/GOES16_TAW_GEOCOLOR_HighRes_Video.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 
+clear
+
 img_dir="./images"
 python_file=$(realpath $(ls | grep 'py'))
 
 if [ ! -d $img_dir ]; then
     mkdir -p $img_dir
     cd $img_dir
-    echo -e "\n Downloading image files..."
+    echo -e "\n Downloading image files...\n"
     python $python_file
+    clear
     echo -e "\n Downloads DONE"
 elif [ -d $img_dir ]; then
     cd $img_dir
-    echo -e "\n Downloading image files..."
+    echo -e "\n Downloading image files...\n"
     python $python_file
+    clear
     echo -e "\n Downloads DONE"
 fi
 
@@ -27,17 +31,23 @@ v_date="$(date +%Y)y$(date +%j)d$(date +%V)w__$(date +%b)_$(date +%d)__$(date +%
 v_name="${v_date}__GOES16-ABI-taw-GEOCOLOR"
 
 ### H264_NVENC
-echo -e "\n Rendering h.264 video."
+echo -e "\n RENDERING h.264 video..."
 f_name="${v_name}__h264_nvenc.mp4"
-ffmpeg -hide_banner -loglevel quiet -hwaccel cuda -pattern_type glob -i "*_GOES16-ABI-taw-GEOCOLOR-7200x4320.jpg" -vf scale=4096:-1 -c:v h264_nvenc -preset p7 -tune lossless -level 6.2 -rc vbr -pix_fmt yuv444p -r 30 $dir/${f_name} 1> /dev/null
-if [ -f $f_name ]; then
-    echo -e "\n File saved as:\n  $f_name in $dir"
+f_path=$dir/${f_name}
+ffmpeg -hide_banner -loglevel quiet -hwaccel cuda -pattern_type glob -i "*_GOES16-ABI-taw-GEOCOLOR-7200x4320.jpg" -vf scale=4096:-1 -c:v h264_nvenc -preset p7 -tune lossless -level 6.2 -rc vbr -pix_fmt yuv444p -r 30 ${f_path} 1> /dev/null
+if [ -f $f_path ]; then
+    echo -e "\n File SAVED as:\n  $f_name \n in:\n  $dir"
+elif [ ! -f $f_path ]; then
+    echo -e "\n Saving h.264 FAILED"
 fi
     
 ### HEVC_NVENC
-echo -e "\n Rendering HEVC video."
+echo -e "\n RENDERING hevc video..."
 f_name="${v_name}__hevc_nvenc.hevc"
-ffmpeg -hide_banner -loglevel quiet -hwaccel cuda -pattern_type glob -i "*_GOES16-ABI-taw-GEOCOLOR-7200x4320.jpg" -c:v hevc_nvenc -preset p7 -tune lossless -level 6.2 -rc vbr -pix_fmt yuv444p -r 30 $dir/${f_name} 1> /dev/null
-if [ -f $f_name ]; then
-    echo -e "\n File saved as:\n  $f_name in $dir"
+f_path=$dir/${f_name}
+ffmpeg -hide_banner -loglevel quiet -hwaccel cuda -pattern_type glob -i "*_GOES16-ABI-taw-GEOCOLOR-7200x4320.jpg" -c:v hevc_nvenc -preset p7 -tune lossless -level 6.2 -rc vbr -pix_fmt yuv444p -r 30 ${f_path} 1> /dev/null
+if [ -f $f_path ]; then
+    echo -e "\n File SAVED as:\n  $f_name \n in:\n  $dir"
+elif [ ! -f $f_path ]; then
+    echo -e "\n Saving hevc FAILED"
 fi
